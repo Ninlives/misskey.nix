69e2baac63d0169964327934984c1aea7cd8939f
mlatus <wqseleven@gmail.com>
2023-07-31 15:47:46 +0800

    bump version 13.14.2

--- package.nix
+++ package.nix
@@ -37,16 +37,15 @@ mkPnpmPackage {
     owner = "misskey-dev";
     repo = "misskey";
     fetchSubmodules = true;
-    rev = "7093662ce5c17a8096c33712d0056de9fd4b5a41";
-    sha256 = "sha256-rFowRU8wJHx1uCs68cpY5wxTsioLtzgzfpgZZzE2A5I=";
+    rev = "a8d45d4b0d24e0c422d4e6d8feab57035239db56";
+    sha256 = "sha256-M+O1pDn1gnpheY8f4PCsUdWCvJcXq4+XDXSJTQhZyAQ=";
   };
-  patches = [ ./config_files_dir_env.patch ];
+  patches = [ ./config_files_dir_env.patch ./build-log.patch ];
   lockOverride.packages = {
     "github.com/misskey-dev/browser-image-resizer/0227e860621e55cbed0aabe6dc601096a7748c4a".resolution.integrity = "sha512-g/obrtD0QNgDQEYw9R+Pdyw9GtGYCbAh5y6XG/TXyShCHP62o8hzQsSJ+VDRp2/qPLWCxApyJA/IpQ6dOncA2g==";    
-    "github.com/misskey-dev/buraha/92b20c1ab15c5cb5a224cf3b1ecd4f6baca12b7c".resolution.integrity = "sha512-TDkaeS+H6xj9S2FtTosQoVweEjtF+CqNjwMdn+y9SWaz5+7HgahtCVFFTN/ltvx8jJd5lMcgAik/WxFSkCG9jw==";
     "github.com/misskey-dev/sharp-read-bmp/02d9dc189fa7df0c4bea09330be26741772dac01".resolution.integrity = "sha512-uv9YdN1KS6queU2iY+RMS3U2IhCSn/zDDoZBqzUp1SmJNnCgynfrguiilYkric/aJx7yBKjuDYUEipNsQ1ePBw==";
-    "github.com/misskey-dev/storybook-addon-misskey-theme/cf583db098365b2ccc81a82f63ca9c93bc32b640(@storybook/blocks@7.0.18)(@storybook/components@7.0.18)(@storybook/core-events@7.0.18)(@storybook/manager-api@7.0.18)(@storybook/preview-api@7.0.18)(@storybook/theming@7.0.18)(@storybook/types@7.0.18)(react-dom@18.2.0)(react@18.2.0)".resolution.integrity = "sha512-QaH1uZSlApQ2CZPkHfhmNm89I92L02s3MdbUPG66TmAyqMaqzxd/AvobORBjtTZ0ymUSa3ii482dRXi+fFb19w==";
-    "github.com/misskey-dev/summaly/77dd5654bb82280b38c1f50e51a771c33f3df503".resolution.integrity = "sha512-j7pj7nkEwiYQbveiwV7WDZ9WcB++e58vKlsAlpRS16sFqt47pxgHDmmrY48cEOI+qUOeDKipG9f6lYSOxnruqA==";
+    "github.com/misskey-dev/storybook-addon-misskey-theme/cf583db098365b2ccc81a82f63ca9c93bc32b640(@storybook/blocks@7.0.27)(@storybook/components@7.1.0)(@storybook/core-events@7.0.27)(@storybook/manager-api@7.0.27)(@storybook/preview-api@7.0.27)(@storybook/theming@7.0.27)(@storybook/types@7.0.27)(react-dom@18.2.0)(react@18.2.0)".resolution.integrity = "sha512-QaH1uZSlApQ2CZPkHfhmNm89I92L02s3MdbUPG66TmAyqMaqzxd/AvobORBjtTZ0ymUSa3ii482dRXi+fFb19w==";
+    "github.com/misskey-dev/summaly/089a0ad8e8c780e5c088b1c528aa95c5827cbdcc".resolution.integrity = "sha512-MXio3ndxVgVPmFblnWL8VliFrfww/rca33VhisLTrMk/zEC20x1eZKhloWE6qPBA8TK5ZfaAdMJij1MFTJMuAg==";
   };
   extraBuildInputs = [ vips ];
 
